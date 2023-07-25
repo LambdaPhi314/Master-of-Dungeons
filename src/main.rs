9fdd74755a79e7e25885b46883b1cca9ef79eaaf
Ragnar-D5 <erik.scheidig@gmx.de>
2023-07-25 17:09:13 +0200

    Kommentare entfernt

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-// Copyright 2019 The Druid Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-//! An example of a custom drawing widget.
-//! We draw an image, some text, a shape, and a curve.
-
-// On Windows platform, don't show a console when opening the app.
 #![windows_subsystem = "windows"]
 
 use druid::FileDialogOptions;
@@ -82,7 +64,6 @@ impl Widget<AppData> for Grid {
             Event::MouseDown(mouse) => {
                 ctx.set_active(true);
                 self.last_grab_point = mouse.pos;
-                // dbg!(mouse);
                 ctx.request_paint();
             },
             Event::MouseUp(_mouse) => {
@@ -90,7 +71,6 @@ impl Widget<AppData> for Grid {
                     ctx.set_active(false);
                     ctx.request_paint();
                 }
-                //dbg!(mouse);
             },
             Event::MouseMove(mouse) => {
                 if ctx.is_active() {
@@ -298,18 +278,6 @@ pub fn main() {
 
 fn build_root_widget() -> impl Widget<AppData> {
     let grid = InfGrid::new();
-    // let button = Button::new("Make config window")
-    //     .on_click(|ctx: &mut EventCtx, data: &mut AppData, env: &Env| {
-    //         ctx.new_sub_window(
-    //             WindowConfig::default()
-    //                 .show_titlebar(false)
-    //                 .window_size(Size::new(100., 100.)),
-    //             Slider::new(),
-    //             data.clone(),
-    //             env.clone(),
-    //         );
-    //     })
-    //     .center();
     let slider = Slider::new()
         .with_range(1.0, 100.0)
         .lens(AppData::square_side);
